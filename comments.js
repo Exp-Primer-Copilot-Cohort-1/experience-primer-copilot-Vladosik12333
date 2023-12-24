ec93693521b120b078405c23619b0ad10682ee25
Vlad <vvllaadd1233345@gmail.com>
2023-12-24 01:27:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,31 @@
-const http = require('http');
+// Create web server application
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Import routes
+const comments = require('./routes/comments');
+
+// Create an express application
+const app = express();
+
+// Use morgan for logging
+app.use(morgan('combined'));
+
+// Use body parser to parse JSON bodies
+app.use(bodyParser.json());
+
+// Use CORS
+app.use(cors());
+
+// Use routes
+app.use(comments);
+
+// Start the server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
